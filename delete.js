31ff072fb364a8a8153babd09a5dac2529cbdeef
In Hwa Na <n10851879@qut.edu.au>
2025-10-16 01:23:15 +1000

    first commit

---
+++ delete.js
@@ -0,0 +1,150 @@
+//const express = require('express')
+const ffmpeg = require('fluent-ffmpeg')
+const S3 = require("@aws-sdk/client-s3");
+const S3Presigner = require("@aws-sdk/s3-request-presigner");
+const { Upload } = require("@aws-sdk/lib-storage")
+const { PassThrough } = require('stream');
+const { DeleteObjectCommand } = require("@aws-sdk/client-s3");
+
+//SQS
+const SQS = require("@aws-sdk/client-sqs");
+const sqsQueueUrl = "https://sqs.ap-southeast-2.amazonaws.com/901444280953/n10851879-test-queue";
+
+const client = new SQS.SQSClient({
+  region: "ap-southeast-2",
+});
+
+
+//Default
+// const app = express ();
+// app.use(express.static('client'));
+// app.use(express.json()) // For parsing json
+
+//AWS
+const bucketName = 'n10851879-test'
+s3Client = new S3.S3Client({ region: 'ap-southeast-2'})
+
+
+
+
+// Read Messgae from queue
+
+async function main() {
+
+
+  try{
+    const receiveCommand = new SQS.ReceiveMessageCommand({
+      MaxNumberOfMessages: 1,
+      QueueUrl: sqsQueueUrl,
+      WaitTimeSeconds: 20, // how long to wait for a message before returning if none.
+      VisibilityTimeout: 20, // overrides the default for the queue?  
+   });
+
+
+    const receiveResponse = await client.send(receiveCommand);
+    console.log("Receiving a message", receiveResponse);
+
+    // If there are no messages then you'll still get a result back.
+    Messages = receiveResponse.Messages;
+    if (!Messages) {
+       console.log("No messages");
+       return;
+    }
+
+
+
+    // Retrieve the first message from the body
+    console.log("Message contents:", Messages[0].Body);
+    const body = JSON.parse(Messages[0].Body);
+    const filename = body.filename
+    console.log("Filename " +  filename)
+    await transcode(filename);
+    // for (const message of data.Messages) {
+    //   const body = JSON.parse(message.Body);
+    //   const s3Key = body.s3Key;
+
+    // Delete the message after dealt with.
+   const deleteCommand = new SQS.DeleteMessageCommand({
+      QueueUrl: sqsQueueUrl,
+      ReceiptHandle: Messages[0].ReceiptHandle,
+   });
+   const deleteResponse = await client.send(deleteCommand);
+   console.log("Deleting the message", deleteResponse);
+  }catch{
+    //Error Message
+  }  
+}
+
+
+
+
+
+
+async function transcode(filename){
+    // Get from S3
+    let transcodedkey = `transcoded${filename}`
+    let response
+    try {
+        response = await s3Client.send(
+            new S3.GetObjectCommand({
+                Bucket: bucketName,
+                Key: filename,
+            }))
+    const video = response.Body
+
+    const videostream = new PassThrough()
+
+    //Creating Upload, uploading mp4 video
+    const uploads3 = new Upload({
+        client: s3Client,
+        params: {
+            Bucket: bucketName,
+            Key:transcodedkey,
+            Body: videostream,
+            ContentType: 'video/mp4'
+        }
+    })
+
+    // Transcoding Using FFMPEG
+    ffmpeg(video)
+    .outputOptions('-movflags frag_keyframe+empty_moov') // Used because MP4 does not work well with streams
+    .videoCodec('libx264')
+    .format('mp4')
+    .on('error', (err) => {
+    console.error('Error:', err.message);
+    return;
+    })
+    .pipe(videostream, {end: true})
+
+    // Start Uploading
+    await uploads3.done()
+
+    /*
+    // Create a pre-signed URL for reading an object
+    const command = new S3.GetObjectCommand({
+            Bucket: bucketName,
+            Key: transcodedkey,
+            ResponseContentDisposition: 'attachment; filename="transcodedvideo.mp4"', // Used for directly downloading from presigned URL
+        });
+    const downloadpresignedURL = await S3Presigner.getSignedUrl(s3Client, command, {expiresIn: 3600} );
+    res.json({url :downloadpresignedURL})*/
+
+    // Delete Original Video    
+    const data = await s3Client.send(new DeleteObjectCommand({
+        Bucket: bucketName,
+        Key: filename
+    }));
+    console.log("Success. Object deleted.", data);
+    // Delete Original Video 
+
+    }catch (err) {
+        console.log(err);
+    }
+
+
+
+}
+
+
+main()
+
